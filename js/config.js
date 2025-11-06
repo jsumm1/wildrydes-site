5987992713dbc76fadef101d113e3cb4f234aaa9
jamaal <jsumm.info@yahoo.com>
2025-11-06 01:27:56 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,9 +5,10 @@ window._config = {
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://80bc8mdcne.execute-api.us-east-2.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
