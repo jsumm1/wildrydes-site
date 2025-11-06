21ee2980b1a9046e69baffc5da00862c6c24ca2c
jamaal <jsumm.info@yahoo.com>
2025-11-06 00:51:48 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_COKQSaatr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2l046ov409vj79qel6s55qkc86', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_U66xkH6v7', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6qur82tg8ktljq8elj3189kjav', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
